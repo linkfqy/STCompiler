60c5d5de11dfe608163a46b6483027491db822ea
linkfqy <linkfqy@qq.com>
2022-10-11 18:26:59 +0800

    finished lab2

--- src/cn/edu/hitsz/compiler/parser/SyntaxAnalyzer.java
+++ src/cn/edu/hitsz/compiler/parser/SyntaxAnalyzer.java
@@ -1,14 +1,13 @@
 package cn.edu.hitsz.compiler.parser;
 
-import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.lexer.Token;
-import cn.edu.hitsz.compiler.parser.table.LRTable;
-import cn.edu.hitsz.compiler.parser.table.Production;
-import cn.edu.hitsz.compiler.parser.table.Status;
+import cn.edu.hitsz.compiler.parser.table.*;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Stack;
 
 //TODO: 实验二: 实现 LR 语法分析驱动程序
 
@@ -23,6 +22,8 @@ import java.util.List;
 public class SyntaxAnalyzer {
     private final SymbolTable symbolTable;
     private final List<ActionObserver> observers = new ArrayList<>();
+    private Iterator<Token> it_token;
+    private LRTable table;
 
 
     public SyntaxAnalyzer(SymbolTable symbolTable) {
@@ -75,18 +76,11 @@ public class SyntaxAnalyzer {
     }
 
     public void loadTokens(Iterable<Token> tokens) {
-        // TODO: 加载词法单元
-        // 你可以自行选择要如何存储词法单元, 譬如使用迭代器, 或是栈, 或是干脆使用一个 list 全存起来
-        // 需要注意的是, 在实现驱动程序的过程中, 你会需要面对只读取一个 token 而不能消耗它的情况,
-        // 在自行设计的时候请加以考虑此种情况
-        throw new NotImplementedException();
+        it_token=tokens.iterator();
     }
 
     public void loadLRTable(LRTable table) {
-        // TODO: 加载 LR 分析表
-        // 你可以自行选择要如何使用该表格:
-        // 是直接对 LRTable 调用 getAction/getGoto, 抑或是直接将 initStatus 存起来使用
-        throw new NotImplementedException();
+        this.table=table;
     }
 
     public void run() {
@@ -94,6 +88,38 @@ public class SyntaxAnalyzer {
         // 你需要根据上面的输入来实现 LR 语法分析的驱动程序
         // 请分别在遇到 Shift, Reduce, Accept 的时候调用上面的 callWhenInShift, callWhenInReduce, callWhenInAccept
         // 否则用于为实验二打分的产生式输出可能不会正常工作
-        throw new NotImplementedException();
+//        throw new NotImplementedException();
+        Stack<Status> stk_status = new Stack<>();
+        stk_status.add(table.getInit());
+        Stack<Term> stk_term = new Stack<>();
+        for (Token token = it_token.next();true;){
+            Status status = stk_status.peek();
+            Action action = table.getAction(status,token);
+            switch (action.getKind()) {
+                case Shift -> {
+                    callWhenInShift(status,token);
+                    stk_term.push(token.getKind());
+                    stk_status.push(action.getStatus());
+                    token = it_token.next();
+                }
+                case Reduce -> {
+                    Production production = action.getProduction();
+                    callWhenInReduce(status, production);
+                    List<Term> body = production.body();
+                    for (int i=body.size()-1;i>=0;i--) {
+                        assert body.get(i).equals(stk_term.peek());
+                        stk_term.pop();
+                        stk_status.pop();
+                    }
+                    stk_term.push(production.head());
+                    stk_status.push(table.getGoto(stk_status.peek(),production.head()));
+                }
+                case Accept -> {
+                    callWhenInAccept(status);
+                    return;
+                }
+                case Error -> throw new RuntimeException("SyntaxAnalyzer: Error when parse");
+            }
+        }
     }
 }
