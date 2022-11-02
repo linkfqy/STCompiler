ec1efed472b194fba1aca7cf7d3032aa85077598
linkfqy <linkfqy@qq.com>
2022-11-02 23:19:28 +0800

    refactored SemanticAnalyzer

--- src/cn/edu/hitsz/compiler/parser/SemanticAnalyzer.java
+++ src/cn/edu/hitsz/compiler/parser/SemanticAnalyzer.java
@@ -3,19 +3,14 @@ package cn.edu.hitsz.compiler.parser;
 import cn.edu.hitsz.compiler.lexer.Token;
 import cn.edu.hitsz.compiler.parser.table.Production;
 import cn.edu.hitsz.compiler.parser.table.Status;
-import cn.edu.hitsz.compiler.parser.table.Term;
 import cn.edu.hitsz.compiler.symtab.SourceCodeType;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 
 import java.util.Stack;
 
-// TODO: 实验三: 实现语义分析
 public class SemanticAnalyzer implements ActionObserver {
-    private record TermStr(Term term,String str){
-
-    }
     private SymbolTable symbolTable;
-    private final Stack<TermStr> stkTermStr=new Stack<>();
+    private final Stack<String> stkName =new Stack<>();
 
     @Override
     public void whenAccept(Status currentStatus) {
@@ -23,16 +18,18 @@ public class SemanticAnalyzer implements ActionObserver {
 
     @Override
     public void whenReduce(Status currentStatus, Production production) {
-        if (production.index() == 4) {
-            symbolTable.get(stkTermStr.get(stkTermStr.size() - 1).str).setType(SourceCodeType.Int);
-        }
-        production.body().forEach((Term term) -> stkTermStr.pop());
-        stkTermStr.push(new TermStr(production.head(),null));
+        if (production.index() == 4) { // S -> D id;
+            var id = stkName.pop();
+            stkName.pop();
+            symbolTable.get(id).setType(SourceCodeType.Int);
+        }else
+            production.body().forEach((var term) -> stkName.pop());
+        stkName.push(null);
     }
 
     @Override
     public void whenShift(Status currentStatus, Token currentToken) {
-        stkTermStr.push(new TermStr(currentToken.getKind(),currentToken.getText()));
+        stkName.push(currentToken.getText());
     }
 
     @Override
